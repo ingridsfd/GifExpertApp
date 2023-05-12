fdf0acaa2ac8411a6a0faa758c3e3a17c10dba25
Ingrid Fonoy <ingridsfonoyd@gmail.com>
2023-05-12 17:03:19 -0600

    GiphyCreditsAdded

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { AddCategory, GifGrid } from './components';
+import Giphylogo from './imgs/Poweredby_100px-White_VertText.png';
 
 export const GifExpertApp = () => {
 	//mantener un hook de React para mantener el estado
@@ -18,8 +19,12 @@ export const GifExpertApp = () => {
 	return (
 		<>
 			{/* titulo */}
-			<h1>GifExpertApp</h1>
-			<h2>@Ingridsfd</h2>
+			<h1>Gif Search App Practice</h1>
+			<img alt="Giphy-credits" src={Giphylogo} />
+			<h4>
+				@Ingridsfd. Go! Try to look for some gifs! Something like: Sailor Moon
+				or Snorlax!
+			</h4>
 			{/* input cada componente debe tener una responsabilidad unica*/}
 			<AddCategory
 				// setCategories={setCategories}
